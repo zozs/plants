9ec325d405100a4294978adf0add8b740faacedc
Linus Karlsson <linus@zozs.se>
2018-04-07 00:08:51 +0200

    Rewrote backend to use promises, and various cleanup.

--- plants.js
+++ plants.js
@@ -1,233 +1,160 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var async = require('async');
-var jadeStatic = require('connect-jade-static');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const jadeStatic = require('connect-jade-static')
+const path = require('path')
+const util = require('util')
+
 // create application/json parser
-var jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json()
 
-var port = process.env.PORT || 9811
+const port = process.env.PORT || 9811
 
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('plants.sqlite');
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('plants.sqlite')
+db.allP = util.promisify(db.all)
+db.getP = util.promisify(db.get)
+db.runP = util.promisify(db.run)
 
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)");
-  db.run('CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY AUTOINCREMENT, place INTEGER REFERENCES places (id) NOT NULL, name TEXT, interval INTEGER DEFAULT NULL, hidden BOOLEAN NOT NULL DEFAULT 0)');
-  db.run('CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, plant INTEGER REFERENCES plants (id) NOT NULL, d DATE NOT NULL)');
-});
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
+  db.run('CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY AUTOINCREMENT, place INTEGER REFERENCES places (id) NOT NULL, name TEXT, interval INTEGER DEFAULT NULL, hidden BOOLEAN NOT NULL DEFAULT 0)')
+  db.run('CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, plant INTEGER REFERENCES plants (id) NOT NULL, d DATE NOT NULL)')
+})
 
-var router = express.Router()
+const router = express.Router()
 
-var baseUrl = 'api';
+const baseUrl = 'api'
 
-router.get('/', function(req, res) {
-  res.json({message: 'api ready to pleasure you!'});
-});
+router.get('/', (req, res) => res.json({message: 'api ready to pleasure you!'}))
 
-function getPlaces(callback) {
-  var sql = 'SELECT id FROM places';
-  db.all(sql, function(err, rows) {
-    rows = rows.map(function(x) { return x.id; });
-    async.map(rows, getPlace, function(err, results) {
-      callback(err, results);
-    });
-  });
+async function getPlaces () {
+  let placeIds = await db.allP('SELECT id FROM places')
+  return Promise.all(placeIds.map(r => getPlace(r.id)))
 }
 
 router.route('/places')
-  .get(function(req, res) {
-    getPlaces(function(err, result) {
-      res.json({ places: result });
-    });
-  });
+  .get(async (req, res) => {
+    res.json({ places: await getPlaces() })
+  })
 
-function getPlace(placeId, callback) {
-  async.series({
-    id: function(callback) {
-      callback(null, placeId);
-    },
-    url: function(callback) {
-      callback(null, baseUrl + '/places/' + placeId);
-    },
-    name: function(callback) {
-      var sql = 'SELECT name FROM places WHERE id=?';
-      db.get(sql, [placeId], function(err, row) {
-        if (row) {
-          callback(err, row.name);
-        } else {
-          callback(err, row);
-        }
-      });
-    },
-    plants: function(callback) {
-      getPlants(placeId, callback);
+async function getPlace (placeId) {
+  let row = await db.getP('SELECT name FROM places WHERE id=?', [placeId])
+  if (row) {
+    let place = {
+      id: placeId,
+      url: baseUrl + '/places/' + placeId,
+      name: row.name,
+      plants: await getPlants(placeId)
     }
-  }, function(err, results) {
-    callback(err, results);
-  });
+    return place
+  } else {
+    throw new Error('No such plant!')
+  }
 }
 
 router.route('/places/:place_id')
-  .get(function(req, res) {
-    getPlace(req.params.place_id, function(err, result) {
-      res.json({place: result});
-    });
-  });
+  .get(async (req, res) => {
+    res.json({ place: await getPlace(req.params.place_id) })
+  })
 
-function getPlants(placeId, plantsCallback) {
-  var sql = 'SELECT id FROM plants WHERE place=?';
-  db.all(sql, [placeId], function(err, rows) {
-    rows = rows.map(function(x) { return x.id; });
-    async.map(rows, getPlant, function(err, results) {
-      plantsCallback(err, results);
-    });
-  });
+async function getPlants (placeId) {
+  let plantIds = await db.allP('SELECT id FROM plants WHERE place=?', [placeId])
+  return Promise.all(plantIds.map(r => getPlant(r.id)))
 }
 
 router.route('/places/:place_id/plants')
-  .get(function(req, res) {
-    getPlants(req.params.place_id, function(err, rows) {
-      res.json({ plants: rows});
-    });
+  .get(async (req, res) => {
+    res.json({ plants: await getPlants(req.params.place_id) })
   })
-  .post(jsonParser, function (req, res) {
-    var sql = 'INSERT INTO plants (name, place, hidden) VALUES (?, ?, 0)';
-    db.run(sql, [req.body.name, req.params.place_id], function (err) {
+  .post(jsonParser, (req, res) => { // TODO: promisify, but tricky since we need this.lastID
+    const sql = 'INSERT INTO plants (name, place, hidden) VALUES (?, ?, 0)'
+    db.run(sql, [req.body.name, req.params.place_id], async function (err) {
       if (err) {
-        res.status(400).end();
+        res.status(400).end()
       } else {
-        getPlant(this.lastID, function (err, plant) {
-          res.json({ plant: plant });
-        });
+        res.json({ plant: await getPlant(this.lastID) })
       }
-    });
-  });
+    })
+  })
 
-function getPlant(plantId, callback) {
-  async.series({
-    id: function(callback) {
-      callback(null, plantId);
-    },
-    url: function(callback) {
-      callback(null, baseUrl + '/plants/' + plantId);
-    },
-    name: function(callback) {
-      var sql = 'SELECT name FROM plants WHERE id=?';
-      db.get(sql, [plantId], function (err, row) {
-        if (row) {
-          callback(err, row.name);
-        } else {
-          callback(err, row);
-        }
-      });
-    },
-    hidden: function(callback) { /* Yeah, inefficient. Merge with above? */
-      var sql = 'SELECT hidden FROM plants WHERE id=?';
-      db.get(sql, [plantId], function (err, row) {
-        if (row) {
-          callback(err, row.hidden == 1);
-        } else {
-          callback(err, row);
-        }
-      });
-    },
-    dates: function(callback) {
-      getDates(plantId, callback);
-    }
-  }, function(err, results) {
-    callback(err, results);
-  });
+async function getPlant (plantId) {
+  const { hidden, name } = await db.getP('SELECT name, hidden FROM plants WHERE id=?', [plantId])
+  return {
+    id: plantId,
+    url: baseUrl + '/plants/' + plantId,
+    name: name,
+    hidden: !!hidden,
+    dates: await getDates(plantId)
+  }
 }
 
 router.route('/plants/:plant_id')
-  .get(function(req, res) {
-    getPlant(req.params.plant_id, function(err, plant) {
-      res.json({ plant: plant });
-    });
+  .get(async (req, res) => {
+    res.json({ plant: await getPlant(req.params.plant_id) })
   })
-  .put(jsonParser, function (req, res) {
-    var sql = 'UPDATE plants SET name=?,hidden=? WHERE id=?';
-    db.run(sql, [req.body.name, !!req.body.hidden, req.params.plant_id], function (err) {
-      if (err) {
-        res.status(400).end();
-      } else {
-        res.status(204).end();
-      }
-    });
-  })
-  .delete(function (req, res) {
-    // delete plant including its events.
-    var plantId = req.params.plant_id;
-    var sql1 = 'DELETE FROM events WHERE plant=?';
-    var sql2 = 'DELETE FROM plants WHERE id=?';
-    db.run(sql1, [plantId], function (err) {
-      if (err) {
-        res.status(400).end();
-      } else {
-        db.run(sql2, [plantId], function (err) {
-          if (err) {
-            res.status(400).end();
-          } else {
-            res.status(204).end();
-          }
-        });
-      }
-    });
-  });
-
-function getDates(plantId, callback) {
-  var sql = 'SELECT d FROM events WHERE plant=? ORDER BY d';
-  db.all(sql, [plantId], function(err, rows) {
-    if (err) {
-      callback(err, null);
-    } else {
-      var dates = rows.map(function(x) { return x.d; });
-      callback(null, dates);
+  .put(jsonParser, async (req, res) => {
+    try {
+      const sql = 'UPDATE plants SET name=?,hidden=? WHERE id=?'
+      await db.runP(sql, [req.body.name, !!req.body.hidden, req.params.plant_id])
+      res.sendStatus(204)
+    } catch (e) {
+      res.sendStatus(400)
     }
-  });
+  })
+  .delete(async (req, res) => {
+    // delete plant including its events.
+    const plantId = req.params.plant_id
+    try {
+      await db.runP('DELETE FROM events WHERE plant=?', [plantId])
+      await db.runP('DELETE FROM plants WHERE id=?', [plantId])
+      res.sendStatus(204)
+    } catch (e) {
+      res.sendStatus(400)
+    }
+  })
+
+async function getDates (plantId) {
+  let rows = await db.allP('SELECT d FROM events WHERE plant=? ORDER BY d', [plantId])
+  return rows.map(r => r.d)
 }
 
 router.route('/plants/:plant_id/dates')
-  .get(function(req, res) {
-    getDates(req.params.plant_id, function(err, dates) {
-      res.json({ dates: dates });
-    });
+  .get(async (req, res) => {
+    let dates = await getDates(req.params.plant_id)
+    res.json({ dates: dates })
   })
-  .post(jsonParser, function(req, res) {
-    var sql = 'INSERT INTO events (plant, d) VALUES (?,?)';
-    db.run(sql, [req.params.plant_id, req.body.date], function (err) {
-      if (err) {
-        res.status(400).end();
-      } else {
-        res.status(204).end();
-      }
-    });
-  });
+  .post(jsonParser, async (req, res) => {
+    const sql = 'INSERT INTO events (plant, d) VALUES (?,?)'
+    try {
+      await db.runP(sql, [req.params.plant_id, req.body.date])
+      res.sendStatus(204)
+    } catch (e) {
+      res.sendStatus(400)
+    }
+  })
 
 router.route('/plants/:plant_id/dates/:date')
-  .delete(function (req, res) {
-    var sql = 'DELETE FROM events WHERE plant=? AND id IN (' +
-      'SELECT id FROM events e WHERE plant=? AND e.d=? LIMIT 1)';
-    db.run(sql, [req.params.plant_id, req.params.plant_id, req.params.date], function (err) {
-      if (err) {
-        res.status(400).end();
-      } else {
-        res.status(204).end();
-      }
-    });
-  });
+  .delete(async (req, res) => {
+    const sql = `DELETE FROM events WHERE plant=? AND id IN (
+      SELECT id FROM events e WHERE plant=? AND e.d=? LIMIT 1)`
+    try {
+      await db.runP(sql, [req.params.plant_id, req.params.plant_id, req.params.date])
+      res.sendStatus(204)
+    } catch (e) {
+      res.sendStatus(400)
+    }
+  })
 
-app.use('/' + baseUrl, router);
+app.use('/' + baseUrl, router)
 // Serve static files.
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(jadeStatic({
-  baseDir: __dirname + '/public',
+  baseDir: path.join(__dirname, '/public'),
   baseUrl: '/',
   maxAge: 86400
-}));
+}))
 
-app.listen(port);
-console.log('Plants server listening on port ' + port);
-
+app.listen(port)
+console.log('Plants server listening on port ' + port)
