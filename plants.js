ba31fa3449fbfdb8d11e307d4d34e24f674cb118
Linus Karlsson <linus.karlsson@eit.lth.se>
2019-06-25 17:42:26 +0200

    Switch to using PostgreSQL as database.

--- plants.js
+++ plants.js
@@ -1,31 +1,30 @@
+require('dotenv').config()
+
 const express = require('express')
-const bodyParser = require('body-parser')
 const path = require('path')
 const pRouter = require('express-promise-router')
-const util = require('util')
+
+const { Pool, types } = require('pg')
 
 const app = express()
-const jsonParser = bodyParser.json()
-
+const jsonParser = express.json()
+const pool = new Pool()
 const port = process.env.PORT || 9811
 
-const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database('plants.sqlite')
-db.allP = util.promisify(db.all)
-db.getP = util.promisify(db.get)
-db.runP = util.promisify(db.run)
+// Disable node-postgres automatic convertion of DATE column to Javascript Date-object
+const TYPE_DATE = 1082
+types.setTypeParser(TYPE_DATE, date => date)
 
-db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)')
-  db.run('CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY AUTOINCREMENT, place INTEGER REFERENCES places (id) NOT NULL, name TEXT, interval INTEGER DEFAULT NULL, hidden BOOLEAN NOT NULL DEFAULT 0)')
-  db.run('CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY AUTOINCREMENT, plant INTEGER REFERENCES plants (id) NOT NULL, d DATE NOT NULL)')
-})
-
-const router = pRouter()
+;(async () => {
+  await pool.query('CREATE TABLE IF NOT EXISTS places (id SERIAL, name TEXT NOT NULL, PRIMARY KEY (id))')
+  await pool.query('CREATE TABLE IF NOT EXISTS plants (id SERIAL, place INTEGER REFERENCES places (id) NOT NULL, name TEXT, interval INTEGER DEFAULT NULL, hidden BOOLEAN NOT NULL DEFAULT FALSE, PRIMARY KEY (id))')
+  await pool.query('CREATE TABLE IF NOT EXISTS events (id SERIAL, plant INTEGER REFERENCES plants (id) NOT NULL, d DATE NOT NULL, PRIMARY KEY (id))')
+})()
 
 const baseUrl = 'api'
+const router = pRouter()
 
-router.get('/', (req, res) => res.json({message: 'api ready to pleasure you!'}))
+router.get('/', (req, res) => res.json({ message: 'api ready to pleasure you!' }))
 
 function NotFoundError (message) {
   this.message = message
@@ -35,22 +34,21 @@ NotFoundError.prototype = Object.create(Error.prototype)
 NotFoundError.prototype.name = 'NotFoundError'
 
 async function getPlaces () {
-  let placeIds = await db.allP('SELECT id FROM places')
-  return Promise.all(placeIds.map(r => getPlace(r.id)))
+  const { rows } = await pool.query('SELECT id FROM places')
+  return Promise.all(rows.map(r => getPlace(r.id)))
 }
 
-router.route('/places')
-  .get(async (req, res) => {
-    res.json({ places: await getPlaces() })
-  })
+router.get('/places', async (req, res) => {
+  res.json({ places: await getPlaces() })
+})
 
 async function getPlace (placeId) {
-  let row = await db.getP('SELECT name FROM places WHERE id=?', [placeId])
-  if (row) {
+  const { rows } = await pool.query('SELECT name FROM places WHERE id=$1', [ placeId ])
+  if (rows.length === 1) {
     return {
       id: placeId,
-      url: baseUrl + '/places/' + placeId,
-      name: row.name,
+      url: `${baseUrl}/places/${placeId}`,
+      name: rows[0].name,
       plants: await getPlants(placeId)
     }
   } else {
@@ -58,38 +56,36 @@ async function getPlace (placeId) {
   }
 }
 
-router.route('/places/:place_id')
-  .get(async (req, res) => {
-    res.json({ place: await getPlace(req.params.place_id) })
-  })
+router.get('/places/:place_id', async (req, res) => {
+  res.json({ place: await getPlace(req.params.place_id) })
+})
 
 async function getPlants (placeId) {
-  let plantIds = await db.allP('SELECT id FROM plants WHERE place=?', [placeId])
-  return Promise.all(plantIds.map(r => getPlant(r.id)))
+  const { rows } = await pool.query('SELECT id FROM plants WHERE place=$1', [ placeId ])
+  return Promise.all(rows.map(r => getPlant(r.id)))
 }
 
 router.route('/places/:place_id/plants')
   .get(async (req, res) => {
     res.json({ plants: await getPlants(req.params.place_id) })
   })
-  .post(jsonParser, (req, res) => { // TODO: promisify, but tricky since we need this.lastID
-    const sql = 'INSERT INTO plants (name, place, hidden) VALUES (?, ?, 0)'
-    db.run(sql, [req.body.name, req.params.place_id], async function (err) {
-      if (err) {
-        res.status(400).end()
-      } else {
-        res.json({ plant: await getPlant(this.lastID) })
-      }
-    })
+  .post(jsonParser, async (req, res) => {
+    try {
+      const sql = 'INSERT INTO plants (name, place, hidden) VALUES ($1, $2, 0) RETURNING id'
+      const { 'rows': [ { id } ] } = await pool.query(sql, [ req.body.name, req.params.place_id ])
+      res.json({ plant: await getPlant(id) })
+    } catch (e) {
+      res.sendStatus(400)
+    }
   })
 
 async function getPlant (plantId) {
   try {
-    const { hidden, name } = await db.getP('SELECT name, hidden FROM plants WHERE id=?', [plantId])
+    const { rows: [ { hidden, name } ] } = await pool.query('SELECT name, hidden FROM plants WHERE id=$1', [ plantId ])
     return {
       id: plantId,
-      url: baseUrl + '/plants/' + plantId,
-      name: name,
+      url: `${baseUrl}/plants/${plantId}`,
+      name,
       hidden: !!hidden,
       dates: await getDates(plantId)
     }
@@ -104,8 +100,8 @@ router.route('/plants/:plant_id')
   })
   .put(jsonParser, async (req, res) => {
     try {
-      const sql = 'UPDATE plants SET name=?,hidden=? WHERE id=?'
-      await db.runP(sql, [req.body.name, !!req.body.hidden, req.params.plant_id])
+      const sql = 'UPDATE plants SET name=$1,hidden=$2 WHERE id=$3'
+      await pool.query(sql, [ req.body.name, !!req.body.hidden, req.params.plant_id ])
       res.sendStatus(204)
     } catch (e) {
       res.sendStatus(400)
@@ -114,32 +110,32 @@ router.route('/plants/:plant_id')
   .delete(async (req, res) => {
     // delete plant including its events.
     const plantId = req.params.plant_id
-    await db.runP('DELETE FROM events WHERE plant=?', [plantId])
-    await db.runP('DELETE FROM plants WHERE id=?', [plantId])
+    await pool.query('DELETE FROM events WHERE plant=$1', [ plantId ])
+    await pool.query('DELETE FROM plants WHERE id=$1', [ plantId ])
     res.sendStatus(204)
   })
 
 async function getDates (plantId) {
-  let rows = await db.allP('SELECT d FROM events WHERE plant=? ORDER BY d', [plantId])
+  const { rows } = await pool.query('SELECT d FROM events WHERE plant=$1 ORDER BY d', [ plantId ])
   return rows.map(r => r.d)
 }
 
 router.route('/plants/:plant_id/dates')
   .get(async (req, res) => {
-    let dates = await getDates(req.params.plant_id)
-    res.json({ dates: dates })
+    const dates = await getDates(req.params.plant_id)
+    res.json({ dates })
   })
   .post(jsonParser, async (req, res) => {
-    const sql = 'INSERT INTO events (plant, d) VALUES (?,?)'
-    await db.runP(sql, [req.params.plant_id, req.body.date])
+    const sql = 'INSERT INTO events (plant, d) VALUES ($1, $2)'
+    await pool.query(sql, [ req.params.plant_id, req.body.date ])
     res.sendStatus(204)
   })
 
 router.route('/plants/:plant_id/dates/:date')
   .delete(async (req, res) => {
-    const sql = `DELETE FROM events WHERE plant=? AND id IN (
-      SELECT id FROM events e WHERE plant=? AND e.d=? LIMIT 1)`
-    await db.runP(sql, [req.params.plant_id, req.params.plant_id, req.params.date])
+    const sql = `DELETE FROM events WHERE plant=$1 AND id IN (
+      SELECT id FROM events e WHERE plant=$2 AND e.d=$3 LIMIT 1)`
+    await pool.query(sql, [ req.params.plant_id, req.params.plant_id, req.params.date ])
     res.sendStatus(204)
   })
 
